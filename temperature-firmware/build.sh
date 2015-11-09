ab901c81cb2d8560ca66ed750124ff4f6d3e7f4a
Johannes Carlsson <johannes.car@gmail.com>
2015-11-09 21:14:26 +0100

    Clean up code and add some basic UART test
    Use defines to make the code easier to read, add a delay
    macro (very approximate busy wait) and print a test string
    on the UART @ 9600bps.

--- temperature-firmware/build.sh
+++ temperature-firmware/build.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+set -e
 avr-gcc -g -Os -mmcu=attiny2313 -c main.c 
 avr-gcc -g -mmcu=attiny2313 -o main.elf main.o
 avr-objcopy -j .text -j .data -O ihex main.elf main.hex
 
+# Internal 8MHz osc
+FUSES="-U lfuse:w:0xde:m -U hfuse:w:0xdf:m -U efuse:w:0xff:m"
 
-avrdude -c buspirate -P /dev/ttyUSB0 -p attiny2313 -v -U flash:w:main.hex
-avrdude -c buspirate -P /dev/ttyUSB0 -p attiny2313 -v -U flash:v:main.hex
+# External 8MHz crystal
+#FUSES="-U lfuse:w:0xe4:m -U hfuse:w:0xdf:m -U efuse:w:0xff:m "
+
+# Fuses default (H:FF, E:DF, L:E4)
+avrdude -c buspirate -P /dev/ttyUSB0 -p attiny2313 -v -U flash:w:main.hex $FUSES
+avrdude -c buspirate -P /dev/ttyUSB0 -p attiny2313 -v -U flash:v:main.hex $FUSES
--- temperature-firmware/main.c
+++ temperature-firmware/main.c
@@ -1,25 +1,80 @@
 #include<avr/io.h>
 
-// TODO use these macros instead
+#define F_CPU 8000000 // 8 MHz
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/sleep.h>
+#include <string.h>
+#include <math.h>
+#include <util/delay.h>
+
 #define SW1_BIT (1 << PORTD3)
 #define SW1_BIT_PORT (PORTD)
+#define SW1_BIT_PIN (PIND)
+#define SW1_PRESSED (!(SW1_BIT_PIN & SW1_BIT))
 
 #define LED_D6_BIT (1 << PORTB1)
 #define LED_D6_PORT (PORTB)
 
+#define LED_D6_ON (LED_D6_PORT |= LED_D6_BIT)
+#define LED_D6_OFF (LED_D6_PORT &= ~LED_D6_BIT)
+
+#define DELAY(time_ms) { long loop=time_ms * 500L; while(loop--);}
+
+#define BAUD_RATE 2400
+// From datasheet Table 14-1, page 123
+#define UBBR_VALUE ((F_CPU / (BAUD_RATE * 16L)) - 1)
+
+void setupUART(void)
+{
+  UBRRL = UBBR_VALUE;
+  UCSRB |= _BV(TXEN); // Tx Enable
+  sei(); // Turn on interrupts
+}
+
+// Transmit USART data
+void transmitData(unsigned char data)
+{
+  while ( !( UCSRA & _BV(UDRE)) ); // Wait for empty transmit buffer
+  UDR = data;
+}
+
+void transmitString(char * pString)
+{
+	int index;
+	while(pString[index])
+	{
+		transmitData(pString[index++]);
+	}
+}
+
+const char * testText = "\r\nThis is a test\r\n";
 int main(void)
 {
-  DDRB |= 1 << PORTB1;
-  PORTB |= 1 << PORTB1;
+  long i;
+  DDRB |= _BV(PORTB1);
+  PORTB |= _BV(PORTB1);
+  setupUART();
   while(1)
   {
-    if (PIND & (1 << PORTD3))
+    transmitString(testText);
+    DELAY(1000);
+    if (SW1_PRESSED)
     {
-      PORTB &= ~(1 << PORTB1);
-    }
-    else
-    {
-      PORTB |= 1 << PORTB1;
-    }
+      LED_D6_ON;
+      transmitData('A');
+	}
+	else
+	{
+      LED_D6_OFF;
+      transmitData('B');
+	}
   }
 }
+
+ISR(WDT_OVERFLOW_vect) { }
+
+ISR(USART_UDRE_vect) {}
+
+ISR(USART_RX_vect) {}
