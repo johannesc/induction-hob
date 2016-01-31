915d5853790118c57e49ff8886f637d4d92c27be
Johannes Carlsson <johannes.car@gmail.com>
2016-01-31 11:12:48 +0100

    Adding code to read the TMP102 sensor
    The code uses the AVR310 application note.

--- temperature-firmware/build.sh
+++ temperature-firmware/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 set -e
-avr-gcc -g -Os -mmcu=attiny2313 -c main.c 
-avr-gcc -g -mmcu=attiny2313 -o main.elf main.o
+avr-gcc -g -Os -mmcu=attiny2313 -c main.c USI_TWI_Master.c
+avr-gcc -g -mmcu=attiny2313 -o main.elf main.o USI_TWI_Master.o
 avr-objcopy -j .text -j .data -O ihex main.elf main.hex
 
 # Internal 8MHz osc
@@ -13,4 +13,4 @@ FUSES="-U lfuse:w:0xde:m -U hfuse:w:0xdf:m -U efuse:w:0xff:m"
 
 # Fuses default (H:FF, E:DF, L:E4)
 avrdude -c buspirate -P /dev/ttyUSB0 -p attiny2313 -v -U flash:w:main.hex $FUSES
-avrdude -c buspirate -P /dev/ttyUSB0 -p attiny2313 -v -U flash:v:main.hex $FUSES
+#avrdude -c buspirate -P /dev/ttyUSB0 -p attiny2313 -v -U flash:v:main.hex $FUSES
--- temperature-firmware/main.c
+++ temperature-firmware/main.c
@@ -8,6 +8,17 @@
 #include <string.h>
 #include <math.h>
 #include <util/delay.h>
+#include "USI_TWI_Master.h"
+
+#define MESSAGEBUF_SIZE       4
+#define TWI_CMD_MASTER_READ  0x0
+
+#define TMP102_TEMPERATURE_REGISTER  0x00
+#define TMP102_CONFIGURATION_REGISTER 0x01
+#define TMP102_TLOW_REGISTER 0x02
+#define TMP102_THIGH_REGISTER 0x03
+
+#define TMP102_CONFIG_BITMASK_EM (1<<4)
 
 #define SW1_BIT (1 << PORTD3)
 #define SW1_BIT_PORT (PORTD)
@@ -26,6 +37,8 @@
 // From datasheet Table 14-1, page 123
 #define UBBR_VALUE ((F_CPU / (BAUD_RATE * 16L)) - 1)
 
+#define I2C_ADDRESS 0b1001000
+
 void setupUART(void)
 {
   UBRRL = UBBR_VALUE;
@@ -34,43 +47,95 @@ void setupUART(void)
 }
 
 // Transmit USART data
-void transmitData(unsigned char data)
+void transmitData(const unsigned char data)
 {
   while ( !( UCSRA & _BV(UDRE)) ); // Wait for empty transmit buffer
   UDR = data;
 }
 
-void transmitString(char * pString)
+void transmitString(const char * pString)
 {
-	int index;
+	int index = 0;
 	while(pString[index])
 	{
 		transmitData(pString[index++]);
 	}
 }
 
+void transmitDigit(unsigned char val)
+{
+    val &= 0xF;
+    val += (val < 10) ? '0' : 'A' - 10;
+    transmitData(val);
+}
+
+void transmitHex(unsigned char val)
+{
+    transmitDigit(val >> 4);
+    transmitDigit(val);
+}
+
 const char * testText = "\r\nThis is a test\r\n";
 int main(void)
 {
-  long i;
-  DDRB |= _BV(PORTB1);
-  PORTB |= _BV(PORTB1);
-  setupUART();
-  while(1)
-  {
-    transmitString(testText);
-    DELAY(1000);
-    if (SW1_PRESSED)
+    DDRB |= _BV(PORTB1);
+    PORTB |= _BV(PORTB1);
+    setupUART();
+
+    unsigned char messageBuf[MESSAGEBUF_SIZE];
+    unsigned char res;
+
+    USI_TWI_Master_Initialise();
+
+    int bol = 0;
+    transmitString("Booted\r\n");
+
+    while(1)
     {
-      LED_D6_ON;
-      transmitData('A');
-	}
-	else
-	{
-      LED_D6_OFF;
-      transmitData('B');
-	}
-  }
+        messageBuf[0] = (I2C_ADDRESS<<TWI_ADR_BITS) | (FALSE<<TWI_READ_BIT); // The first byte must always consit of General Call code or the TWI slave address.
+        messageBuf[1] = TWI_CMD_MASTER_READ;             // The first byte is used for commands.
+        res = USI_TWI_Start_Transceiver_With_Data( messageBuf, 2 );
+        if (!res)
+        {
+            transmitString("Read 1st fail\r\n");
+        }
+
+        messageBuf[0] = (I2C_ADDRESS<<TWI_ADR_BITS) | (TRUE<<TWI_READ_BIT); // The first byte must always consit of General Call code or the TWI slave address.
+        do
+        {
+            res = USI_TWI_Start_Transceiver_With_Data( messageBuf, 3 );
+            if (!res)
+            {
+                transmitString("fail\r\n");
+            }
+        }
+        while (USI_TWI_Get_State_Info() == USI_TWI_NO_ACK_ON_ADDRESS);
+
+        unsigned char err = USI_TWI_Get_State_Info();
+
+        //TODO set 13 bit mode?
+        transmitString("Read done");
+        transmitHex(messageBuf[1]);
+        transmitHex(messageBuf[2]);
+        transmitString("=");
+        //Page 8, 15
+        unsigned int temp = ((unsigned int)(messageBuf[1]<<8) | (unsigned int)(messageBuf[2])) >> 4;
+        transmitHex(temp >> 8);
+        transmitHex(temp & 0XFF);
+
+        transmitString("\r\n");
+
+        bol++;
+        if (bol%2)
+        {
+            LED_D6_ON;
+        }
+        else
+        {
+            LED_D6_OFF;
+        }
+            DELAY(10000);
+        }
 }
 
 ISR(WDT_OVERFLOW_vect) { }
